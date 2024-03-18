f22ee746fbf38f38d053826e9a4fe0eb6a17f89a
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-03-18 13:10:00 +0100

    Task 1 - Implement sequences functions

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -25,15 +25,29 @@ object Sequences: // Essentially, generic linkedlists
       case Nil()                 => Nil()
 
     // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
+      case Cons(h, t) if n > 0 => Cons(h, take(t)(n - 1))
+      case _ => Nil()
 
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1, h2), zip(t1, t2))
+      case _ => Nil()
     
-    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
-    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
+    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = l1 match
+      case Cons(h, t) => Cons(h, concat(t, l2))
+      case _ =>l2
 
-    def min(l: Sequence[Int]): Optional[Int] = ???
-    
+    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = l match
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
+      case _ => Nil()
+
+    def min(l: Sequence[Int]): Optional[Int] = l match
+      case Cons(h, t) => min(t) match
+        case Optional.Just(v) if h < v => Optional.Just(h)
+        case Optional.Empty() => Optional.Just(h)
+        case _  => min(t)
+      case _ => Optional.Empty()
+
 @main def trySequences =
   import Sequences.* 
   val l = Sequence.Cons(10, Sequence.Cons(20, Sequence.Cons(30, Sequence.Nil())))
