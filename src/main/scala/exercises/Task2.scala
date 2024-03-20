3062398a3a72bf5b0d28a4c65500820ada3c6ba5
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-03-20 19:41:50 +0100

    Task 2 - Implement functions as extensions of Sequence[A]

--- src/main/scala/exercises/Task2.scala
+++ src/main/scala/exercises/Task2.scala
@@ -5,23 +5,34 @@ import u03.Sequences.*
 import u03.Sequences.Sequence.*
 
 object Task2 extends App:
+
   def getCourses(persons: Sequence[Person]): Sequence[String] =
-    map(filter(persons)(element =>
-      element match
-        case Person.Teacher(_, _) => true
-        case _ => false)
-    )(person => person match
-      case Person.Teacher(_, course) => course
-      case _ => ""
-    )
+    persons.getCoursesExt()
   
   def getCoursesWithFlatMap(persons: Sequence[Person]): Sequence[String] =
-    flatMap(persons)(person => person match
-      case Person.Teacher(_, course) => Cons(course, Nil())
-      case _ => Nil()
-    )
+    persons.getCoursesWithFlatMapExt()
 
-  def foldLeft[A](l: Sequence[A])(defaultValue: A)(operator: (A, A) => A): A = l match
-    case Cons(h, t) => operator(h, foldLeft(t)(defaultValue)(operator))
-    case _ => defaultValue
-  
+  def foldLeft[A](l: Sequence[A])(defaultValue: A)(operator: (A, A) => A): A = 
+    l.foldLeftExt(defaultValue)(operator) 
+
+  extension [A](l: Sequence[A])
+
+    def getCoursesExt(): Sequence[String] =
+      map(filter(l)(element =>
+        element match
+          case Person.Teacher(_, _) => true
+          case _ => false)
+      )(person => person match
+        case Person.Teacher(_, course) => course
+        case _ => ""
+      )
+    
+    def getCoursesWithFlatMapExt(): Sequence[String] =
+      flatMap(l)(person => person match
+        case Person.Teacher(_, course) => Cons(course, Nil())
+        case _ => Nil()
+      )
+    
+    def foldLeftExt(defaultValue: A)(operator: (A, A) => A): A = l match
+      case Cons(h, t) => operator(h, t.foldLeftExt(defaultValue)(operator))
+      case _ => defaultValue
