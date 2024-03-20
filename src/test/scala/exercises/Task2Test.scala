8705ae15ca8540d1f65266fcddfb5b8a6bb80ecb
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-03-20 17:01:57 +0100

    Task 2 - Refactor tests for getCourses()

--- src/test/scala/exercises/Task2Test.scala
+++ src/test/scala/exercises/Task2Test.scala
@@ -9,6 +9,11 @@ class Task2Test:
   import Sequence.*
   import exercises.Task2.*
 
+  val marco: Person.Teacher = Person.Teacher("Marco Rossi", "Matematica")
+  val luca: Person.Teacher = Person.Teacher("Luca Verdi", "Matematica")
+  val mario: Person.Student = Person.Student("Mario Bianchi", 2024)
+  val giovanni: Person.Student = Person.Student("Giovanni Neri", 2019)
+
   @Test
   def testGetCoursesWithEmptySequence() =
     assertEquals(Nil(), getCourses(Nil()))
@@ -16,44 +21,34 @@ class Task2Test:
   @Test
   def testGetCoursesWithOneTeacher() =
     assertEquals(
-      Cons("Matematica", Nil()),
-      getCourses(Cons(Person.Teacher("Marco Rossi", "Matematica"), Nil()))
+      Cons(marco.course, Nil()),
+      getCourses(Cons(marco, Nil()))
     )
 
   @Test
   def testGetCoursesWithMultipleTeachers() =
     assertEquals(
-      Cons("Matematica", Cons("Fisica", Nil())),
-      getCourses(Cons(Person.Teacher("Marco Rossi", "Matematica"), Cons(Person.Teacher("Luca Verdi", "Fisica"), Nil())))
+      Cons(marco.course, Cons(luca.course, Nil())),
+      getCourses(Cons(marco, Cons(luca, Nil())))
     )
   
   @Test
   def testGetCoursesWithStudent() =
     assertEquals(
       Nil(),
-      getCourses(Cons(Person.Student("Mario Bianchi", 2024), Nil()))
+      getCourses(Cons(mario, Nil()))
     )
   
   @Test
   def testGetCoursesWithMultipleStudents() =
     assertEquals(
       Nil(),
-      getCourses(Cons(Person.Student("Mario Bianchi", 2024), Cons(Person.Student("Giovanni Neri", 2019), Nil())))
+      getCourses(Cons(mario, Cons(giovanni, Nil())))
     )
   
   @Test
   def testGetCoursesWithMixedSequence() =
     assertEquals(
-      Cons("Matematica", Cons("Fisica", Nil())),
-      getCourses(
-        Cons(Person.Student("Mario Bianchi", 2024),
-          Cons(Person.Teacher("Marco Rossi", "Matematica"),
-            Cons(Person.Student("Giovanni Neri", 2019),
-              Cons(Person.Teacher("Luca Verdi", "Fisica"),
-                Nil()
-              )
-            )
-          )
-        )
-      )
+      Cons(marco.course, Cons(luca.course, Nil())),
+      getCourses(Cons(mario, Cons(marco, Cons(giovanni, Cons(luca, Nil())))))
     )
