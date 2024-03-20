a5dee78fc7f45a5d8c279086760e4c79c40de26c
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-03-20 17:58:42 +0100

    Task 2 - Implement getCoursesWithFlatMap()

--- src/main/scala/exercises/Task2.scala
+++ src/main/scala/exercises/Task2.scala
@@ -1,9 +1,8 @@
 package exercises
 
-import u03.Sequences.Sequence
-import u03.Sequences.Sequence.map
-import u03.Sequences.Sequence.filter
 import u02.AlgebraicDataTypes.Person
+import u03.Sequences.*
+import u03.Sequences.Sequence.*
 
 object Task2 extends App:
   def getCourses(persons: Sequence[Person]): Sequence[String] =
@@ -14,4 +13,10 @@ object Task2 extends App:
     )(person => person match
       case Person.Teacher(_, course) => course
       case _ => ""
+    )
+  
+  def getCoursesWithFlatMap(persons: Sequence[Person]): Sequence[String] =
+    flatMap(persons)(person => person match
+      case Person.Teacher(_, course) => Cons(course, Nil())
+      case _ => Nil()
     )
